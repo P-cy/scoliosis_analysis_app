b6f1458adde0265abadc3423bfd515d9da943dbc
kwdevteam <developer@kw.ac.th>
2024-11-15 08:27:06 +0700

    อัปเดทเวอร์ชันของแพ็คเกจ

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:scoliosis_analysis_app/pages/first_page.dart';
 
 void main() {
+  //WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
